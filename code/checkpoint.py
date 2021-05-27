8e337a76ba1d1c06ed5d97b05bbcd13f6d1e1278
vim-hjk <hjkim@vim.cau.ac.kr>
2021-05-27 04:16:12 +0000

    Update Hyojin Code

---
+++ code/checkpoint.py
@@ -0,0 +1,40 @@
+import os
+import torch
+from tensorboardX import SummaryWriter
+
+use_cuda = torch.cuda.is_available()
+
+default_checkpoint = {
+    "epoch": 0,
+    "train_losses": [],
+    "train_symbol_accuracy": [],
+    "train_sentence_accuracy": [],
+    "train_wer": [],
+    "validation_losses": [],
+    "validation_symbol_accuracy": [],
+    "validation_sentence_accuracy": [],
+    "validation_wer": [],
+    "lr": [],
+    "grad_norm": [],
+    "model": {},
+    "configs":{},
+    "token_to_id":{},
+    "id_to_token":{},
+}
+
+
+def save_checkpoint(checkpoint, dir="./checkpoints", prefix=""):
+    # Padded to 4 digits because of lexical sorting of numbers.
+    # e.g. 0009.pth
+    filename = "{num:0>4}.pth".format(num=checkpoint["epoch"])
+    if not os.path.exists(os.path.join(prefix, dir)):
+        os.makedirs(os.path.join(prefix, dir))
+    torch.save(checkpoint, os.path.join(prefix, dir, filename))
+
+
+def load_checkpoint(path, cuda=use_cuda):
+    if cuda:
+        return torch.load(path)
+    else:
+        # Load GPU model on CPU
+        return torch.load(path, map_location=lambda storage, loc: storage)
---
+++ code/utils.py
@@ -0,0 +1,92 @@
+import os
+import yaml
+import random
+import torch
+import torch.optim as optim
+import numpy as np
+
+from adamp import AdamP
+from madgrad import MADGRAD
+from easydict import EasyDict
+
+from networks.Attention import Attention
+from networks.SATRN import SATRN
+
+
+def seed_everything(seed: int):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    
+def get_network(
+    model_type,
+    FLAGS,
+    model_checkpoint,
+    device,
+    train_dataset,
+):
+    model = None
+
+    if model_type == "SATRN":
+        model = SATRN(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == "CRNN":
+        model = CRNN()
+    elif model_type == "Attention":
+        model = Attention(FLAGS, train_dataset, model_checkpoint).to(device)
+    else:
+        raise NotImplementedError
+
+    return model
+
+
+def get_optimizer(optimizer, params, lr, weight_decay=None):
+    if optimizer == "Adam":
+        optimizer = optim.Adam(params, lr=lr, weight_decay=weight_decay)
+    elif optimizer == "AdamP":
+        optimizer = AdamP(params, lr=lr, weight_decay=weight_decay)
+    elif optimizer == "MADGRAD":
+        optimizer = MADGRAD(params, lr=lr, weight_decay=0.0)
+    else:
+        raise NotImplementedError
+    return optimizer
+
+
+class YamlConfigManager:
+    def __init__(self, config_file_path='./configs/SATRN.yaml'):
+        super().__init__()
+        self.values = EasyDict()        
+        if config_file_path:
+            self.config_file_path = config_file_path
+            self.reload()
+    
+    def reload(self):
+        self.clear()
+        if self.config_file_path:
+            with open(self.config_file_path, 'r') as f:
+                self.values.update(yaml.safe_load(f))
+
+    def clear(self):
+        self.values.clear()
+    
+    def update(self, yml_dict):
+        for (k1, v1) in yml_dict.items():
+            if isinstance(v1, dict):
+                for (k2, v2) in v1.items():
+                    if isinstance(v2, dict):
+                        for (k3, v3) in v2.items():
+                            self.values[k1][k2][k3] = v3
+                    else:
+                        self.values[k1][k2] = v2
+            else:
+                self.values[k1] = v1
+
+    def export(self, save_file_path):
+        if save_file_path:
+            with open(save_file_path, 'w') as f:
+                yaml.dump(dict(self.values), f)
